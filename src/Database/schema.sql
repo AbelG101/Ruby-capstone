cec1eab4024975ce1c5c5c09f93fe35216c4df7b
AbelG101 <abelgebeyehu101@gmail.com>
2022-11-15 01:00:51 +0300

    Add schema for items table

--- src/Database/schema.sql
+++ src/Database/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE books (
-  id integer GENERATED ALWAYS AS IDENTITY,
-  genre_id integer,
-  author_id integer,
-  source_id integer,
-  label_id integer,
-  publish_date date,
-  archived bit,
-  publisher varchar,
-  cover_state varchar,
-  PRIMARY KEY (id),
-  FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE,
-  FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
-  FOREIGN KEY (source_id) REFERENCES sources(id) ON DELETE CASCADE,
-  FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE
+CREATE DATABASE catalog_of_my_things;
+
+CREATE TABLE items (
+	id int GENERATED ALWAYS AS IDENTITY,
+	genre_id integer,
+	author_id integer,
+	source_id integer,
+	label_id integer,
+	publish_date date,
+	archived bit,
+	PRIMARY KEY (id),
+	FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE,
+	FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
+	FOREIGN KEY (source_id) REFERENCES sources(id) ON DELETE CASCADE,
+	FOREIGN KEY (label_id) REFERENCES labels(id) ON DELETE CASCADE
 );
