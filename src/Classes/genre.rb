982c87832961c5fc137342b4bcf3736f430fe485
Elijah Odjegba <elijahodjegba@gmail.com>
2022-11-15 02:11:40 +0100

    class constructor setup

--- src/Classes/genre.rb
+++ src/Classes/genre.rb
@@ -1,1 +1,13 @@
 class Genre
+  attr_accessor :id, :name, :items
+  
+  def initialize(_id, name)
+    @id = Random.rand(1..499)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+
+  end
+end  
