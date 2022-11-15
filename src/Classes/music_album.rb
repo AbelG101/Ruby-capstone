953fd89e0a787d32d99d3617f11c384576643be4
Elijah Odjegba <elijahodjegba@gmail.com>
2022-11-15 01:46:29 +0100

    visible class properties

--- src/Classes/music_album.rb
+++ src/Classes/music_album.rb
@@ -1,6 +1,9 @@
 require './src/Classes/item'
 
 class MusicAlbum < Item
-  def initialize()
-    super()
+  attr_reader :on_spotify
+
+  def initialize(id, name, publish_date, archived: false, on_spotify: false)
+    super(id, name, publish_date, archived)
+    @on_spotify = on_spotify
   end
