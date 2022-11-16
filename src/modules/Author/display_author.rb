f90698f6119fc0a23bc7ec4dba3cb431c958c626
Lena <summyalena@gmail.com>
2022-11-17 00:29:03 +0100

    displayed the author and game classes

---
+++ src/modules/Author/display_author.rb
@@ -0,0 +1,14 @@
+module DisplayAuthor
+    def display_author
+      puts '----------------'
+      if @persist_authors.length.zero?
+        puts 'No author yet 🕸 ,please add some authors...'
+      else
+        puts 'Game: '
+        @persist_authors.each_with_index do |author, index|
+          puts "#{index}) first_name: '#{author['first_name']}', last_name: '#{author['last_name']}'"
+        end
+      end
+      puts '----------------'
+    end
+  end
---
+++ src/modules/Game/dispalay_game.rb
@@ -0,0 +1,14 @@
+module DisplayGame
+    def display_game
+      puts '----------------'
+      if @persist_games.length.zero?
+        puts 'No games yet 🕸 ,please add some games...'
+      else
+        puts 'Game: '
+        @persist_games.each_with_index do |game, index|
+          puts "#{index}) multiplayer: '#{game['multiplayer']}', last_played_at: '#{game['last_played_at']}'"
+        end
+      end
+      puts '----------------'
+    end
+  end
