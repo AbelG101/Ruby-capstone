107787c6a858ec9568afe30a8238c3f73720ea91
Lena <summyalena@gmail.com>
2022-11-17 00:29:48 +0100

    Modified the app file

--- src/app.rb
+++ src/app.rb
@@ -3,6 +3,10 @@ require_relative './modules/Book/display_books'
 require_relative './modules/Label/display_labels'
 require_relative './modules/preserve_data'
 require_relative './modules/load_data'
+require_relative './modules/Game/dispalay_game.rb'
+require_relative './modules/Author/create_author.rb'
+require_relative './modules/Author/display_author.rb'
+require_relative './modules/Game/persist_data.rb'
 
 class App
   include LoadData
@@ -10,13 +14,20 @@ class App
   include CreateBook
   include DisplayBooks
   include DisplayLabels
+  include DisplayAuthor
+  include DisplayGame
+  include CreateGame
 
   def initialize
     @books = []
     @labels = []
+    @authors = []
+    @games = []
     load_data
+    @persist_authors = load_author
+    @persist_games = load_game
   end
-
+  
   def run(option)
     case option
     when 1
@@ -25,6 +36,12 @@ class App
       display_labels
     when 9
       create_book
+    when 12
+      create_game
+    when 7
+      display_author
+    when 4
+      display_game
     else
       preserve_data
       puts 'Thank you for using our app, see ya👋'
