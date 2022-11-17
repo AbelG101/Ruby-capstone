a15f1455b94436c310113fd8a6b7b2833a3de779
Elijah Odjegba <elijahodjegba@gmail.com>
2022-11-17 17:16:12 +0100

    Add unit tests for music and genre

--- src/spec/genre_spec.rb
+++ src/spec/genre_spec.rb
@@ -1,12 +1,23 @@
-require_relative '../Classes/genre'
+require './src/Classes/genre'
 
 describe Genre do
-  context 'Unit tests for Genre Class' do
+  before :each do
+    @genre = Genre.new('Blues')
+  end
+
+  context 'checks if its properly instantiated' do
     it 'should create a new genre when the class is instantiated' do
-      genre = Genre.new(21, 'Blues')
-      genre.add_item({ one: 1, two: 2 })
-      expect(genre.name).to eq('Blues')
-      expect(genre.items.length).to eq(1)
+      expect(@genre).to be_an_instance_of Genre
+    end
+  end
+
+  context '#add_item' do
+    it 'takes an item object and adds it to the collection of items' do
+      item = double('item')
+      allow(item).to receive(:genre=)
+      genre = Genre.new('Trap')
+      genre.add_item(item)
+      expect(genre.items.length).to eq 1
     end
   end
 end
