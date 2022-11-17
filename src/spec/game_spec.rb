1642a8510447812a27eacbc96b8f4a7ba9ab189a
Lena <summyalena@gmail.com>
2022-11-17 08:19:08 +0100

    Fixed linters error

--- src/spec/game_spec.rb
+++ src/spec/game_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../Classes/game.rb'
 
 describe Game do
-    context 'would test Game class file' do
-     it 'should test the game class file' do
-        @game = Game.new(true, '2022-11-10', '2022-11-09')
-    expect(@game.multiplayer).to eql(true)
-    expect(@game.last_played_at.year).to eql (2022)
-     end
+  context 'would test Game class file' do
+    it 'should test the game class file' do
+      @game = Game.new(true, '2022-11-10', '2022-11-09')
+      expect(@game.multiplayer).to eql(true)
+      expect(@game.last_played_at.year).to eql (2022)
     end
+  end
 end
