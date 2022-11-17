4d5d9b4eb13bfb691c54f8f8d23b61a609929aaa
Lena <summyalena@gmail.com>
2022-11-17 08:43:55 +0100

    Added the author tests

--- src/Classes/author.rb
+++ src/Classes/author.rb
@@ -1,15 +1,15 @@
-class author
-    attr_accessor :name, :first_name, :last_name
-    def initialize(name, first_name, last_name)
-        @id = Random.rand(1..100)
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+class Author
+  attr_accessor :name, :first_name, :last_name
 
-    def add_item(item)
-        @items << item
-        item.add_author(self) 
-    end
+  def initialize(name, first_name, last_name)
+    @id = Random.rand(1..100)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 
+  def add_item(item)
+    @items << item
+    item.add_author(self)
+  end
 end
