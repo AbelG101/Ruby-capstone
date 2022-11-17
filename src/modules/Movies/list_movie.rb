ce03061c5cc838972e0d58178b18cedd9ebf74e4
Christabelle143 <christabelleyouta143@gmail.com>
2022-11-17 06:41:49 +0100

    add module for listing the movies

---
+++ src/modules/Movies/list_movie.rb
@@ -0,0 +1,8 @@
+module ListMovie
+def list_movie
+    puts 'List of all movie:'
+    puts 'No movie recorded yet.' if @movies.empty?
+    @movies.each do |movie|
+      puts "movie name: #{movie.name},\nPublished date: #{movie.publish_date},\nsilent #{movie.silent}\n\n"
+    end
+  end
