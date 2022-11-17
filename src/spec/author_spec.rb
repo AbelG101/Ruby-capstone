3e18d4a1ca6523d941d9b131fe794cb5a40acdf0
Lena <summyalena@gmail.com>
2022-11-17 09:44:30 +0100

    made corrections

--- src/spec/author_spec.rb
+++ src/spec/author_spec.rb
@@ -1,12 +1,12 @@
-require_relative '../Classes/author.rb'
+require_relative '../Classes/author'
 
 describe Authors do
   context 'would test Author class file' do
     it 'should test the author class file' do
       @author = Author.new('Assumpta', 'Alena', 'Okolike')
-      expect(@author.name).to eq ('Assumpta')
-      expect(@author.first_name).to eq ('Alena')
-      expect(@author.last_name).to eq ('Okolike')
+      expect(@author.name).to eq('Assumpta')
+      expect(@author.first_name).to eq('Alena')
+      expect(@author.last_name).to eq('Okolike')
     end
   end
 end
