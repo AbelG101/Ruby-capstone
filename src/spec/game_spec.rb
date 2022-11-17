8ebc74a48bb5702fc393b910817f1a2163a509c5
Lena <summyalena@gmail.com>
2022-11-17 10:37:55 +0100

    made corrections

--- src/spec/game_spec.rb
+++ src/spec/game_spec.rb
@@ -5,7 +5,7 @@ describe Games do
     it 'should test the game class file' do
       @game = Game.new(true, '2022-11-10', '2022-11-09')
       expect(@game.multiplayer).to eql(true)
-      expect(@game.last_played_at.year).to eql (2022)
+      expect(@game.last_played_at.year).to eql(2022)
     end
   end
 end
